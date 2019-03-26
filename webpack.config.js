abac5d86084b13ce6d9f56f714c29ed485bd04f3
Mateusz Starakiewicz <ms.starakiewicz@gmail.com>
2019-03-26 22:11:15 +0100

    fix2

--- webpack.config.js
+++ webpack.config.js
@@ -1,17 +1,10 @@
 const path = require('path');
 
+//webpack.config.js
 module.exports = {
     entry: './src/app.js',
-        output: {
+    output: {
         path: path.resolve(__dirname, 'build'),
         filename: 'app.bundle.js'
-    },
-    module: {
-        rules: [
-            {
-                test: /\.js$/,
-                loader: "babel-loader"
-            }
-        ]
     }
 };
